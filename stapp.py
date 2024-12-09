a1329b160603ccd49403580dd559f58de9179efe
Aman78600 <105182914+Aman78600@users.noreply.github.com>
2024-12-09 22:37:23 +0530

    Update stapp.py

--- stapp.py
+++ stapp.py
@@ -3,7 +3,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-pro",
     temperature=0.1,
-    api_key="AIzaSyCzbg4K-P_iNGY8OlLsAqIKN29M9AauMu4"
+    api_key="AIzaSyDMOC1kuWVNsYlrT5VcMSiPIzqyA-JYsV8"
 )
 question=st.text_input('enter question.')
 responce=llm.predict(question)
